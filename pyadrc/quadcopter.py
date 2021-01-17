d53648b7ef160dbd7eacae7e04c8bf57c1081f93
Ongun Türkcüoglu <30687798+onguntoglu@users.noreply.github.com>
2021-01-17 07:33:46 +0100

    Update quadcopter.py

--- pyadrc/quadcopter.py
+++ pyadrc/quadcopter.py
@@ -7,40 +7,43 @@ import pyadrc
 
 def main():
 
-    dadrc = pyadrc.ADRC.DiscreteLinearADRC(order=2, delta=0.001,
-                                           b0=0.028, t_settle=2,
-                                           k_eso=10, eso_init=False,
-                                           half_gain=(False, False),
-                                           inc_form=True)
+    dadrc = pyadrc.ADRC.adrc_ss(order=2, delta=0.001, b0=0.028, t_settle=1,
+                                k_eso=10, eso_init=False,
+                                rate_lim=(-5, 5),
+                                magnitude_lim=(-100, 300),
+                                half_gain=(False, False))
 
-    quadcopter = pyadrc.QuadAltitude(delta=0.001)
+    quadcopter = pyadrc.QuadAltitude()
 
-    ylist = list()
-    ulist = list()
+    _u, _y = [], []
 
     u = 0
     counter = 0
 
-    ylist.append(0)
-    ulist.append(0)
+    _u.append(0)
+    _y.append(0)
 
-    while counter < 100000:
+    while counter < 10000:
         y = quadcopter(u)
-        u = dadrc(y, u, 1)
+
+        # if counter > 5000:
+        #    y = y + 2
+
+        u = dadrc(y, u, 5)
         counter = counter + 1
 
-        ylist.append(y)
-        ulist.append(u)
+        _y.append(y)
+        _u.append(u)
 
     plt.figure()
     plt.subplot(2, 1, 1)
-    plt.plot(ylist, ds='steps')
+    plt.plot(_y, ds='steps')
     plt.title('Output')
     plt.xlabel('Amplitude')
     plt.ylabel('Samples')
 
     plt.subplot(2, 1, 2)
-    plt.plot(ulist, ds='steps')
+    plt.plot(_u, ds='steps')
     plt.title('Input')
     plt.xlabel('Amplitude')
     plt.ylabel('Samples')
