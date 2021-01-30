49f3f5caca048446c354c3ef1de767676ecc7bee
onguntoglu <ongun.turkcuoglu@campus.tu-berlin.de>
2021-01-30 22:59:05 +0100

    conf fixed

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+import pyadrc
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ---------------------------------------------
--- pyadrc/__init__.py
+++ pyadrc/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Ongun Türkcüoglu"""
 __email__ = 'onguntoglu@gmail.com'
 __version__ = '0.1.0'
 
-from .pyadrc import adrc
+from .pyadrc import *
